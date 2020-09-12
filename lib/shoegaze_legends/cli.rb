ab1574b6bc68f8432a4d705250e241abeb80d77c
dcoppage86 <dakotakcoppage@me.com>
2020-09-12 11:15:04 -0600

    building the code i want

--- lib/shoegaze_legends/cli.rb
+++ lib/shoegaze_legends/cli.rb
@@ -4,20 +4,20 @@ class ShoegazeLegends::CLI
         # puts "*******************************************************"
         puts "* Welcome User...to Shoegaze Legends...Are you ready? *"
         # puts "*******************************************************"
-        #get_album_name
-        #get_artist_of_album
+        get_album_list
+        get_user_input
         #give_info_on_album
     end
 
-    # def get_album_list
-    #     #scrape
-    #     @albums = [""] #['1-10', '11-20', '21-30', '31-40', '41-50']
-    # end
+    def get_album_list
+        #scrape
+        @albums = ["My Bloody Valentine - Loveless", "Ride - Nowhere", "Slowdive - Souvlaki", "Jesus and Mary Chain - Psychocandy", "Cocteau Twins - Heaven Or Las Vegas", "Verve - A Storm In Heaven", "Swervedriver - Mezcal Head", "Catherine Wheel - Chrome", "Lush - Spooky", "Chapterhouse - Whirlpool"]
+    end
 
-    # def get_user_input
-    #     @albums.each.with_index(1) do |albums, index| 
-    #         puts "#{index}. #{album}"
-    #     end
-    # end
+    def get_user_input
+        @albums.each.with_index(1) do |album, index| 
+            puts "#{index}. #{album}"
+        end
+    end
 
 end
