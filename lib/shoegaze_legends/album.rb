1c8056787a9b3c448f8714a093e00db998beec91
dcoppage86 <dakotakcoppage@me.com>
2020-09-12 11:24:59 -0600

    building initial classes

--- lib/shoegaze_legends/album.rb
+++ lib/shoegaze_legends/album.rb
@@ -1,3 +1,19 @@
 class ShoegazeLegends::Album
+    attr_accessor :name, :artist
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @artist = artist
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.save
+        @@all << self
+    end
 
 end
