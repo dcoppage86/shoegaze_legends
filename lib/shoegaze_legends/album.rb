fbb97df01ec70dd850134065bf1ed3ecd5b963e8
dcoppage86 <dakotakcoppage@me.com>
2020-09-13 01:48:29 -0600

    building scraper class and still experimenting with starting the program

--- lib/shoegaze_legends/album.rb
+++ lib/shoegaze_legends/album.rb
@@ -1,11 +1,13 @@
 class ShoegazeLegends::Album
-    attr_accessor :name, :artist
+    attr_accessor :name, :artist, :url, :album_info
 
     @@all = []
 
     def initialize(name)
         @name = name
         @artist = artist
+        @url = url
+        @album_info = album_info
     end
 
     def self.all
--- lib/shoegaze_legends/scraper.rb
+++ lib/shoegaze_legends/scraper.rb
@@ -1,3 +1,12 @@
 class ShoegazeLegends::Scraper
 
+    def scrape_album
+        doc = Nokogiri::HTML(open("https://blog.discogs.com/en/10-of-the-best-shoegaze-records"/))    
+    end
+
+
+    def scrape_album_info
+
+    end
+
 end
