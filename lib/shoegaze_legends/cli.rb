bfd6610bce8183a0f0e6eaf544525445a070bac9
dcoppage86 <dakotakcoppage@me.com>
2020-09-14 11:10:38 -0600

    refactored methods to be more concise

--- lib/shoegaze_legends/cli.rb
+++ lib/shoegaze_legends/cli.rb
@@ -6,45 +6,47 @@ class ShoegazeLegends::CLI
         puts "* Welcome to Shoegaze Legends, The Top 10 Best Shoegaze albums of all time *"
         puts "****************************************************************************"
         puts ""
-        # get_album_list
-        # get_user_input
-        #give_info_on_album
-        start
+        get_albums
+        list_albums
+        # get_user_album
+        get_user_input
+        # start
     end
 
-    def start
-        puts "**********************************************************************************************"
-        puts "Now lets stop shoegazing and get to the point of our journey here. Are you ready? Enter Y or N"
-        puts "**********************************************************************************************"
-        input = gets.strip
-
-        puts "**********************************************************************************************"
-        puts "Select an number between 1-10."
-        puts "**********************************************************************************************"
-        input = gets.strip
-
-        if input == "y"
-            start
-        elsif input == "n"
-            puts ""
-            puts "See you later...back to reality!"
-            exit
-        else
-            puts ""
-            puts "Where are you trying to go with this?"
-            start
-        end
-    end
-
-    def get_album_list
+    def get_albums
         #scrape
         @albums = ["My Bloody Valentine - Loveless", "Ride - Nowhere", "Slowdive - Souvlaki", "Jesus and Mary Chain - Psychocandy", "Cocteau Twins - Heaven Or Las Vegas", "Verve - A Storm In Heaven", "Swervedriver - Mezcal Head", "Catherine Wheel - Chrome", "Lush - Spooky", "Chapterhouse - Whirlpool"]
+        # binding.pry
     end
 
-    def get_user_input
+    def list_albums
+        puts "**********************************"
+        puts "* Select a number between 1-10. *"
+        puts "**********************************"
         @albums.each.with_index(1) do |album, index| 
             puts "#{index}. #{album}"
         end
     end
+
+    # def get_user_album
+    #     user_selection = gets.strip.to_i
+    #     method(user_selection) if valid?(user_selection, @albums)
+    # end
+    # refactored into the get_user_input method
+
+    def valid?(input, data)
+        input.to_i <= data.length && input.to_i > 0
+    end
+
+    def get_user_input
+        user_selection = gets.strip.to_i
+        if user_selection.between?(1, 10)
+            album = @albums[user_selection - 1]
+            album.get_albums
+            puts "#{album}"
+        end
+        get_albums
+    end
+            
 
 end
