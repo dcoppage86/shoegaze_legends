7c57c0554fb214a66ec7d0fa6a509455ddddc1ac
Collin Jilbert <cjilbert504@gmail.com>
2021-12-23 12:43:28 -0600

    Remove useless failing test

--- spec/shoegaze_legends_spec.rb
+++ spec/shoegaze_legends_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe ShoegazeLegends do
   it "has a version number" do
     expect(ShoegazeLegends::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
